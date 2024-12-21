27be2fa52ec4b5ca2d2fb9c892f263a637b4c965
松浦 知也 Matsuura Tomoya <me@matsuuratomoya.com>
2024-12-22 00:27:41 +0900

    updated config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
 import { defineConfig } from "vite";
-import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 import { resolve } from "node:path";
 import dts from "vite-plugin-dts";
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
-  plugins: [wasm(), topLevelAwait(), dts()],
+  plugins: [topLevelAwait(), dts(), checker({ types: "tsc" })],
   build: {
     minify: true,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "mimium_webaudio",
-      formats: ["iife"],
+      formats: ["es", "umd"],
       fileName: () => "mimium-webaudio.js",
     },
     sourcemap: true,
   },
   compilerOptions: {
     tsconfigPath: resolve(__dirname, "tsconfig.json"),
-  }
+  },
 });
